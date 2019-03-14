6a2d94add9af95b95a236a14fe944a67bef10424
Janice Medina <compass2k17@outlook.com>
2019-03-14 18:10:23 -0500

    Add checklist task data

---
+++ src/garageData.js
@@ -0,0 +1,36 @@
+const garageData = [
+  {
+    id: 1,
+    task: 'Move everything out of the garage',
+    completed: false,
+  },
+  {
+    id: 2,
+    text: 'Sweep ceiling, corners, walls, and inside of garage door',
+    completed: false,
+  },
+  {
+    id: 3,
+    text:
+      'Spray cleaner on cloth and wipe down mounted shelves, light switches, door jambs, door knobs, and window frames',
+    completed: false,
+  },
+  {
+    id: 4,
+    text: 'Clean windows inside and out',
+    completed: false,
+  },
+  {
+    id: 5,
+    text: 'Sweep floor or use leaf blower',
+    completed: false,
+  },
+  {
+    id: 6,
+    text:
+      'Sort, toss or mark for donation as you return items to garage',
+    completed: false,
+  },
+];
+
+export default garageData;
